3a724a1da1578f7e2a27a1f90344ed5c5290f995
magidc <ricardo.rodcas@gmail.com>
2025-12-05 12:16:51 +0100

    More types added

--- src/main/kotlin/com/magidc/ideavim/anyobject/handlers/AnyConditionalHandler.kt
+++ src/main/kotlin/com/magidc/ideavim/anyobject/handlers/AnyConditionalHandler.kt
@@ -3,9 +3,12 @@ package com.magidc.ideavim.anyobject.handlers
 import com.magidc.ideavim.anyobject.handlers.base.AbstractTSBasedHandler
 
 class AnyConditionalHandler : AbstractTSBasedHandler() {
-    override val innerBlockTypes = setOf("block", "switch_block_statement_group", "expression_statement", "case_statement", "compound_statement", "call_expression")
+    override val innerBlockTypes = setOf(
+        "block", "switch_block_statement_group", "expression_statement", "case_statement",
+        "compound_statement", "call_expression", "return_statement"
+    )
     override val targetTypes = setOf(
         "try_statement", "if_statement", "if_expression", "match_expression", "switch_statement", "switch_expression", "when_expression",
-        "unless_statement", "branch_expression"
+        "unless_statement", "branch_expression", "try_with_resources_statement"
     )
 }
