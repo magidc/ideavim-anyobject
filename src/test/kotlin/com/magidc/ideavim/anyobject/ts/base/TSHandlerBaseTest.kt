9b7bebcadbfe1eca28fadde4c24cfa274ad112bc
magidc <ricardo.rodcas@gmail.com>
2025-12-21 13:25:36 +0100

    Test method per language

--- src/test/kotlin/com/magidc/ideavim/anyobject/ts/base/TSHandlerBaseTest.kt
+++ src/test/kotlin/com/magidc/ideavim/anyobject/ts/base/TSHandlerBaseTest.kt
@@ -4,12 +4,14 @@ import com.google.common.collect.HashMultimap
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.maddyhome.idea.vim.api.VimEditor
 import com.magidc.ideavim.anyobject.handlers.base.TSBasedHandler
 import org.apache.commons.lang3.StringUtils
 import org.junit.Assert
 import java.io.File
+import java.io.FileInputStream
 import java.io.InputStreamReader
+import java.net.URI
+import java.net.URL
 
 abstract class TSHandlerBaseTest(val handler: TSBasedHandler) : BasePlatformTestCase() {
     companion object {
@@ -33,12 +35,39 @@ abstract class TSHandlerBaseTest(val handler: TSBasedHandler) : BasePlatformTest
         val expected: String
     )
 
-    private lateinit var testFilePaths: List<String>
-    private val handlerName = handler.javaClass.simpleName.lowercase().removeSuffix("handler")
-    private val testFailResults = HashMultimap.create<TestData, TestFailResult>()
+    private class TestFailResults {
+        private val results = HashMultimap.create<TestData, TestFailResult>()
 
-    private fun getTestData(testFilePath: String): TestData {
-        val inputStream = javaClass.classLoader.getResourceAsStream(testFilePath) ?: throw IllegalArgumentException("Test data not found at $testFilePath")
+        fun add(testData: TestData, inner: Boolean, caretId: String, caretPosition: Int, actual: String, expected: String) {
+            results.put(testData, TestFailResult(inner, caretId, caretPosition, actual, expected))
+        }
+
+        fun assertResults() {
+            if (!results.isEmpty) {
+                for (entry in results.asMap()) {
+                    val testData = entry.key
+                    println("Failed on test data: ${testData.filePath}")
+                    println("Code:\n${testData.code}")
+                    println("---------------------------------------")
+                    println("Code with carets:\n${testData.codeWithCarets}")
+                    println("---------------------------------------")
+                    entry.value.map {
+                        "${if (it.inner) "Inner" else "Around"} selection at caret ${it.caretId} [${it.caretPosition}].\n" +
+                                "Expected:\n${it.expected}\n" +
+                                "Actual:\n${it.actual}"
+                    }.forEach { println(it) }
+                    println("---------------------------------------")
+                }
+                Assert.fail()
+            }
+        }
+    }
+
+    private val handlerName = handler.javaClass.simpleName.lowercase().removeSuffix("handler")
+    private lateinit var testDataDir: URL
+
+    private fun getTestData(testFileURI: URI): TestData {
+        val inputStream = FileInputStream(File(testFileURI))
         val json = Gson().fromJson(InputStreamReader(inputStream), JsonObject::class.java)
         val extension = json.get("extension").asString
         val inner = json.getAsJsonObject("inner").entrySet().associate { it.key to it.value.asString }
@@ -58,53 +87,57 @@ abstract class TSHandlerBaseTest(val handler: TSBasedHandler) : BasePlatformTest
     override fun setUp() {
         super.setUp()
         val basePath = "testData/$handlerName"
-        val testDataDir = javaClass.classLoader.getResource(basePath) ?: throw IllegalArgumentException("Test data not found")
+        testDataDir = javaClass.classLoader.getResource(basePath) ?: throw IllegalArgumentException("Test data not found")
         TSBasedHandler.setDocumentCacheMaxSize(0)
-
-        testFilePaths = File(testDataDir.path).listFiles()
-            ?.filter { it.extension == "json" }
-            ?.map { "$basePath/${it.name}" }
-            ?.sorted() ?: emptyList()
-
     }
 
-    fun testHandler() {
-        testFilePaths.forEach { doTestHandler(getTestData(it), true) }
-        testFilePaths.forEach { doTestHandler(getTestData(it), false) }
-        if (!testFailResults.isEmpty) {
-            for (entry in testFailResults.asMap()) {
-                val testData = entry.key
-                println("Failed on test data: ${testData.filePath}")
-                println("Code:\n${testData.code}")
-                println("---------------------------------------")
-                println("Code with carets:\n${testData.codeWithCarets}")
-                println("---------------------------------------")
-                entry.value.map {
-                    "${if (it.inner) "Inner" else "Around"} selection at caret ${it.caretId} [${it.caretPosition}].\n" +
-                            "Expected:\n${it.expected}\n" +
-                            "Actual:\n${it.actual}"
-                }.forEach { println(it) }
-                println("---------------------------------------")
-            }
-            Assert.fail()
+    fun doTestLanguageHandler(fileSuffix: String = ".json") {
+        val testFileNames = File(testDataDir.path).listFiles { it.name.contains(fileSuffix) }
+            ?.map { it.name }
+            ?.sorted() ?: return
+
+        val testFailResults = TestFailResults()
+        testFileNames.forEach {
+            val testFileURI = testDataDir.toURI().resolve(it)
+            if (!File(testFileURI).exists()) return
+            val testData = getTestData(testFileURI)
+            doTestHandler(testData, true, testFailResults)
+            doTestHandler(testData, false, testFailResults)
         }
+        testFailResults.assertResults()
     }
 
-    private fun doTestHandler(testData: TestData, inner: Boolean) {
+    private fun doTestHandler(testData: TestData, inner: Boolean, testFailResults: TestFailResults) {
         val assertions = if (inner) testData.inner else testData.around
         val editor = MockVimEditor(testData.code, testData.filePath, 0)
 
         for (caretPositionEntry in testData.caretPositions.entries) {
             editor.currentCaretOffset = caretPositionEntry.value
             val expected = assertions[caretPositionEntry.key] ?: continue
-            val actual = executeHandlerSelection(editor, inner)
+            //TODO: Test wrapper for TSBasedHandlers that collects unused targetTypes after all test execution, in order to clean up the lists. The same with innerBlock types
+            val actual = handler.findSelection(editor, inner, 1)?.let { editor.text().substring(it.startOffset, it.endOffset) } ?: ""
+
             if (actual != expected)
-                testFailResults.put(testData, TestFailResult(inner, caretPositionEntry.key, caretPositionEntry.value, actual, expected))
+                testFailResults.add(testData, inner, caretPositionEntry.key, caretPositionEntry.value, actual, expected)
         }
     }
 
-    private fun executeHandlerSelection(editor: VimEditor, inner: Boolean): String {
-        val textRange = handler.findSelection(editor, inner, 1) ?: return ""
-        return editor.text().substring(textRange.startOffset, textRange.endOffset)
-    }
+    fun testAll() = doTestLanguageHandler()
+    fun testClojure() = doTestLanguageHandler("clojure.json")
+    fun testCpp() = doTestLanguageHandler("cpp.json")
+    fun testCsharp() = doTestLanguageHandler("csharp.json")
+    fun testGo() = doTestLanguageHandler("go.json")
+    fun testJava() = doTestLanguageHandler("java.json")
+    fun testJavascript() = doTestLanguageHandler("javascript.json")
+    fun testKotlin() = doTestLanguageHandler("kotlin.json")
+    fun testObjectivec() = doTestLanguageHandler("objectivec.json")
+    fun testPhp() = doTestLanguageHandler("php.json")
+    fun testPython() = doTestLanguageHandler("python.json")
+    fun testR() = doTestLanguageHandler("r.json")
+    fun testRuby() = doTestLanguageHandler("ruby.json")
+    fun testRust() = doTestLanguageHandler("rust.json")
+    fun testScala() = doTestLanguageHandler("scala.json")
+    fun testSql() = doTestLanguageHandler("sql.json")
+    fun testSwift() = doTestLanguageHandler("swift.json")
+    fun testTypescript() = doTestLanguageHandler("typescript.json")
 }
