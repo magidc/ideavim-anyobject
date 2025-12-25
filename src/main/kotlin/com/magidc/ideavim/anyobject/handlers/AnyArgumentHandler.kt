715f0ab50311b390f2284e2c1460acb86dcdc06c
magidc <ricardo.rodcas@gmail.com>
2025-12-25 12:01:44 +0100

    Testing AnyArgument

--- src/main/kotlin/com/magidc/ideavim/anyobject/handlers/AnyArgumentHandler.kt
+++ src/main/kotlin/com/magidc/ideavim/anyobject/handlers/AnyArgumentHandler.kt
@@ -4,6 +4,6 @@ class AnyArgumentHandler : AnyItemHandler() {
     override val targetTypes = super.targetTypes + setOf(
         "arguments", "argument_list", "value_arguments", "formal_parameters", "formal_parameters_list",
         "parameters", "parameter_list", "function_parameters", "primary_constructor", "function_value_parameters",
-        "lambda_parameters", "lambda_parameter_list", "parameter_declaration", "formal_parameter", "set"
+        "lambda_parameters", "lambda_parameter_list", "formal_parameter"
     )
 }
--- src/main/kotlin/com/magidc/ideavim/anyobject/handlers/AnyItemHandler.kt
+++ src/main/kotlin/com/magidc/ideavim/anyobject/handlers/AnyItemHandler.kt
@@ -11,7 +11,8 @@ open class AnyItemHandler : TSBasedHandler() {
 
     override val targetTypes: Set<String> = setOf(
         "array", "array_initializer", "list", "tuple", "initializer_expression", "composite_literal",
-        "literal_value", "dictionary", "set", "element_list", "sequence", "collection", "object", "array_literal"
+        "literal_value", "dictionary", "set", "element_list", "sequence", "collection", "object", "array_literal",
+        "tuple_expression", "token_tree"
     )
 
     override val acceptNode: (TSNode) -> Boolean = { n -> n.grammarType == "pair" || !n.parent.isNull && n.isNamed && targetTypes.contains(n.parent.grammarType) }
