40b465f809c3d25e0986e0ed0554fe01fca4e5d4
magidc <ricardo.rodcas@gmail.com>
2026-01-08 01:55:46 +0100

    Tests passed: AnyString and AnyVariable AnyConditional inner selection is unstable

--- src/main/kotlin/com/magidc/ideavim/anyobject/handlers/AnyConditionalHandler.kt
+++ src/main/kotlin/com/magidc/ideavim/anyobject/handlers/AnyConditionalHandler.kt
@@ -1,27 +1,18 @@
 package com.magidc.ideavim.anyobject.handlers
 
-import com.maddyhome.idea.vim.common.TextRange
 import com.magidc.ideavim.anyobject.handlers.base.TSBasedHandler
-import com.magidc.ideavim.anyobject.utils.TSDocument
-import org.treesitter.TSNode
 
 class AnyConditionalHandler : TSBasedHandler() {
 
     override val innerBlockTypes = setOf(
-        "switch_block_statement_group", "switch_section", "case_statement", "block",
-        "return_statement", "control_structure_body", "when_entry", "match_arm", "default_statement", "else_clause", "case_clause",
-        "compound_statement", "statement_block", "switch_case", "then"
+        "switch_block_statement_group", "switch_section", "case_statement", "block", "else",
+        "return_statement", "when_entry", "match_arm", "default_statement", "case_clause", "else_clause", "braced_expression",
+        "statement_block", "switch_case", "then", "switch_default", "statements", "switch_entry", "default_case", "expression_case"
     )
 
     override val targetTypes = setOf(
-        "match_statement", "switch_statement", "switch_expression", "when_expression", "begin", "case",
-        "try_statement", "if_statement", "if_expression", "unless_statement", "branch_expression", "try_with_resources_statement"
+        "match_statement", "match_expression", "switch_statement", "switch_expression", "when_expression", "begin", "case", "try_expression",
+        "try_statement", "if_statement", "if_expression", "unless_statement", "branch_expression", "try_with_resources_statement", "do_statement",
+        "expression_switch_statement", "if"
     )
-
-    override fun findInnerBlockRange(currentNode: TSNode, objectNode: TSNode, offset: Int, tsDocument: TSDocument): TextRange {
-        return generateSequence(currentNode) { it.parent.takeIf { n -> !n.isNull } }
-            .firstOrNull { n -> innerBlockTypes.any { n.grammarType == it } }
-            ?.let { getCodeBlock(it, tsDocument) }
-            ?: tsDocument.toTextRange(objectNode)
-    }
 }
